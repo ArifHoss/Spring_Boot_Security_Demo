67628cc943e6675cf9c86474fa0cb5d593a1a9bd
arif <hossaininfo@ymail.com>
2022-03-03 15:25:15 +0100

    logout added to ApplicationSecurityConfig

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserPermission.*;
 import static com.example.demo.security.ApplicationUserRole.*;
@@ -43,9 +46,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll().defaultSuccessUrl("/courses", true)
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe();
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                    .key("SOMETHING_VERY_SECURED")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID","remember-me")
+                .logoutSuccessUrl("/login");
+
     }
 
 
